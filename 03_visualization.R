579a720a4b9c6e7021f0d26a4c3d108052769171
VincenzoBusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2024-03-14 17:14:08 +0100

    Update 03_visualization.R

--- 03_visualization.R
+++ 03_visualization.R
@@ -10,4 +10,27 @@ im.list()
 
 # importare i dati assegnandoli ad una variabile di nome "mato" con:
 mato <- im.import("matogrosso_ast_2006209_lrg.jpg")
-mato
+# im.import() visualizza direttamente l'immagine importata
+
+# importo i dati assegnandoli ad una variabile di nome "b2".
+# con questa immagine si visualizza tutto ciò che riflette la lunghezza d'onda del blu.
+b2 <- im.import("sentinel.dolomites.b2.tif")
+# cambio la scala dei colori con colorRampPalette() in cui passiamo un vettore di più elementi che corrispondono ai colori
+# si possono cambiare le sfumature segnandole nelle parentesi esterne alla funzione
+clg <- colorRampPalette(c("black", "grey", "light grey"))(3)
+plot(b2, col=clg)
+# imposto le sfumature a 100
+clg <- colorRampPalette(c("black", "grey", "light grey"))(100)
+plot(b2, col=clg)
+# cambio la palette di colori da utilizzare
+clg <- colorRampPalette(c("magenta", "royalblue2", "seagreen3", "yellow1"))(100)
+plot(b2, col=clg)
+# importo altre bande
+b3 <- im.import("sentinel.dolomites.b3.tif")
+plot(b3, col=clg)
+
+b4 <- im.import("sentinel.dolomites.b4.tif")
+plot(b4, col=clg)
+
+b8 <- im.import("sentinel.dolomites.b8.tif")
+plot(b8, col=clg)
