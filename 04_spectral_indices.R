3d4714092c2a55cc60277aa7c4ae5773fc12b1f4
VincenzoBusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2024-03-25 10:49:30 +0100

    Update 04_spectral_indices.R

--- 04_spectral_indices.R
+++ 04_spectral_indices.R
@@ -24,3 +24,25 @@ im.plotRGB(m1992, r=2, g=3, b=1)
 
 # importo l'immagine del mato grosso del 2006
 m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+im.plotRGB(m2006, r=1, g=2, b=3)
+# inserisco la banda del NIR al posto della banda del Verde così da ottenere in verde ciò che è la foresta pluviale
+im.plotRGB(m2006, r=2, g=1, b=3)
+# inserisco NIR sulla banda del blu così da mettere in risalto il suolo nudo che verrà rappresentato in giallo; il fiume appare giallo perché l'acqua era torbida
+im.plotRGB(m2006, r=2, g=3, b=1)
+
+# plot delle tre bande per le due immagini
+par(mfrow=c(2,3))
+# NIR su R 1992 
+im.plotRGB(m1992, r=1, g=2, b=3)
+# NIR su G 1992
+im.plotRGB(m1992, r=2, g=1, b=3)
+# NIR su R 1992
+im.plotRGB(m1992, r=2, g=3, b=1)
+# NIR su R 2006
+im.plotRGB(m2006, r=1, g=2, b=3)
+# NIR su G 2006
+im.plotRGB(m2006, r=2, g=1, b=3)
+# NIR su B 2006
+im.plotRGB(m2006, r=2, g=3, b=1)
+
+# creazione di un indice per fare delle differenze quantitative per valutare il cambiamento del paesaggio
