c08aee30b3ecd4c1bde53004528de7e2ee0a3530
VincenzoBusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2024-04-12 12:31:08 +0200

    Update 06_Time_Series.R

--- 06_Time_Series.R
+++ 06_Time_Series.R
@@ -11,7 +11,9 @@ im.list()
 EN01 <- im.import("EN_01.png")
 EN13 <- im.import("EN_13.png")
 
+
 par(mfrow=c(2,1))
+
 # im.plotRGB.auto() visualizza le immagini utilizzando le bande 1,2,3
 im.plotRGB.auto(EN01)
 im.plotRGB.auto(EN13)
@@ -20,3 +22,39 @@ im.plotRGB.auto(EN13)
 difEN = EN01[[1]] - EN13[[1]]
 cl <- colorRampPalette(c("blue","white","red")) (100)
 plot(difEN, col=cl)
+
+## Sciglimento dei ghiacciai in Groenlandia
+# proxy: variabile più facile da misurare e utilizzata al posto di altre
+
+im.list()
+
+g2000 <- im.import("greenland.2000.tif")
+clg <- colorRampPalette(c("black", "blue", "white", "red"))(100)
+plot(g2000, col=clg)
+# è stato impostato il nero per la temperatura più bassa
+
+g2005 <- im.import("greenland.2005.tif")
+g2010 <- im.import("greenland.2010.tif")
+g2015 <- im.import("greenland.2015.tif")
+
+par(mfrow=c(1,2))
+plot(g2000, col=clg)
+plot(g2015, col=clg)
+
+par(mfrow=c(2,2))
+plot(g2000, col=clg)
+plot(g2005, col=clg)
+plot(g2010, col=clg)
+plot(g2015, col=clg)
+
+greenland <- c(g2000, g2005, g2010, g2015)
+plot(stack, col=clg)
+
+
+difg = greenland[[1]] - greenland[[4]]
+clgreen <- colorRampPalette(c("red", "white", "blue"))(100)
+plot(difg, col=clgreen)
+
+im.plotRGB(greenland, r=1, g=2, b=4) # per ogni banda RGB è stata assegnata una immagine di greenland: r=g2000, g=g2005, b=g2015
+
+# tutti questi metodi servono a mantenere dei valori continui. 
