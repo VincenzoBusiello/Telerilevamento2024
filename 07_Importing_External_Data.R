e1fc1d588a7a16b670781e258deefecf4ff29c6f
VincenzoBusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2024-04-23 17:41:38 +0200

    Update 07_Importing_External_Data.R

--- 07_Importing_External_Data.R
+++ 07_Importing_External_Data.R
@@ -15,3 +15,13 @@ im.plotRGB(eclissi, r=1, g=2, b=3)
 dif = eclissi[[1]] - eclissi[[2]]
 plot(dif)
 
+# importing data from Copernicus
+
+soil <- rast("c_gls_SSM1km_202404210000_CEURO_S1CSAR_V1.2.1.nc")
+plot(soil)
+plot(soil[[1]])
+
+# come ritagliare l'immagine della porzione che può interessare definendo la x minima e x massima e y minima e y massima inserendole in un array
+ext <- c(25, 30, 55, 88)
+soilcrop <- crop(soil, ext)
+plot(soilcrop)
