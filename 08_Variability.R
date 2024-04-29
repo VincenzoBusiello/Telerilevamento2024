456ca7371ddf132c75f2e22c7c3e930410bd769a
VincenzoBusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2024-04-29 10:04:33 +0200

    Update 08_Variability.R

--- 08_Variability.R
+++ 08_Variability.R
@@ -9,4 +9,39 @@
 
 
 library(terra) 
+library(imageRy)
+install.packages("viridis")
+library(viridis)
 
+im.list() # apro la lista di immagini all'interno del pacchetto imageRy
+
+sent -> im.import("sentinel.png")
+
+im.plotRGB(sent, 1,2,3) 
+# NIR = banda 1 --> tutto ciò che è vegetazione verrà rappresentato in rosso
+# Red = Banda 2
+# Green = Banda 3
+
+im.plotRGB(sent, 2,1,3)
+
+# Per calcolare la deviazione standard bisogna scegliere una delle variabili, in questo caso uno dei livelli, su cui calcolarla. 
+# Il livello che discrimina di più gli oggetti è il NIR. 
+
+# assegno il livello 1 di sent alla variabile nir
+
+nir <- sent[[1]]
+cl <- colorRampPalette(c("black", "blue", "green", "white"))(100)
+plot(nir, col=cl)
+
+# la funzione focal() viene utilizzata per il calcolo della variabilità attraverso la "moving window". 
+# il secondo argomento della funzione focal() è una matrice esplicata con la funzione matrix() in cui verrà inserito il range di pixel della moving window,
+# numero di pixel per colonna e per riga; il terzo argomento è la funzione di standard deviation sd
+
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
+plot(sd3)
+
+# utilizzo la gamma di colori viridis proveniente dal pacchetto viridi
+viridisc <- colorRampPalette(viridis(7))(256)
+plot(sd3, col=viridisc)
+# la variabilità viene indicata con la differenza di colore: dove è stato rimarcato il giallo vi è maggiore variabilità. Vengono risaltati molto di più i bordi.
+# 
