515c87d6330c8438cb810e26b406fdc409a11798
VincenzoBusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2024-05-02 17:48:13 +0200

    Update 09_Multivariate_Analysis.R

--- 09_Multivariate_Analysis.R
+++ 09_Multivariate_Analysis.R
@@ -31,6 +31,20 @@ im.plotRGB(sentdo, r=3, g=4, b=2)
 # il verde dell'immagine rappresenta la componente NIR
 
 
-# correlazione tra le bande 2, 3, 4, 8. Utilizza l'indice di Pearson e misura la correlazione tra variabili in un range che va da -1 a 1. La correlazione non è causalità! 
 pairs(sentdo)
+# pairs() è la funzione per poter calcolare la correlazione con il metodo di Pearson. Tra le bande blu, verde e rosso c'è un'alta correlazione (0.99 in un range che va da -1 (correlazione negativa) a 1(correlazione positiva))
 # Le immagini in diagonale e in verde mostrano le correlazioni tra le bande. Rosso, Verde e Blu sono correlate tr loro, il NIR ha una correlazione minore con le altre bande.
+
+# Principal Components Analysis
+
+pcimage <- im.pca(sentdo)
+# la prima stringa indica la variabilità di ogni banda
+
+tot <- sum(1315.01800, 605.44274, 44.06567, 30.26080)
+1315.01800 * 100 / tot # calcolo della percentuale della varianza spiegata dal primo asse PC1, il risultato è 65.92%
+605.44274 * 100 / tot # 30.35%
+44.06567 * 100 / tot # 2.21%
+30.26080 * 100 / tot # 1.52%
+
+vir <- colorRampPalette(viridis(7))(100)
+plot(pcimage, col=viridis (100))
