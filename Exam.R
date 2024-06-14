63f311ff8e755d27fa22187d1ae69ee483567e70
vincenzobusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2024-06-14 19:40:09 +0200

    Update Exam.R

--- Exam.R
+++ Exam.R
@@ -8,77 +8,77 @@ library(patchwork)
 setwd("C:/TelExam") # imposto la cartella di lavoro 
 
 # Ad ogni immagine è assegnata una banda
-lb1 <- rast("lb1.jpg") # blue
-lb2 <- rast("lb2.jpg") # green
-lb3 <- rast("lb3.jpg") # red
-lb4 <- rast("lb4.jpg") # NIR
+l17_1 <- rast("l17_1.tiff") #blue
+l17_2 <- rast("l17_2.tiff") #green
+l17_3 <- rast("l17_3.tiff") #red
+l17_4 <- rast("l17_4.tiff") #NIR
+
+l17 <- c(l17_1, l17_2, l17_3, l17_4) #costruisco l'immagine che andrò ad utilizzare assemblandola con le diverse bande.
 
 
-lbdo <- c(lb1,lb2,lb3,lb4) #costruisco l'immagine che andrò ad utilizzare assemblandola con le diverse bande.
 
+d17_1 <- rast("d17_1.tiff") #blue
+d17_2 <- rast("d17_2.tiff") #green
+d17_3 <- rast("d17_3.tiff") #red
+d17_4 <- rast("d17_4.tiff") #NIR
 
-db1 <- rast("db1.jpg") # blue
-db2 <- rast("db2.jpg") # green
-db3 <- rast("db3.jpg") # red
-db4 <- rast("db4.jpg") # NIR
-
-dbdo <- c(db1,db2,db3,db4) #costruisco l'immagine che andrò ad utilizzare assemblandola con le diverse bande.
+d17 <- c(d17_1, d17_2, d17_3, d17_4) #costruisco l'immagine che andrò ad utilizzare assemblandola con le diverse bande.
 
 
 par(mfrow=c(2,1)) #confronto delle due immagini sostituendo la banda del blu con quella NIR
-im.plotRGB(lbdo, 4,3,2)
-im.plotRGB(dbdo, 4,3,2)
+im.plotRGB(l17, 4,3,2)
+im.plotRGB(d17, 4,3,2)
 
 
 # per mettere in risalto lo specchio d'acqua utilizzo l'indice NWDI (Normalized Difference Water Index)
 # tale indice si comporta come l'NDVI, quindi ha valori massimi e minimi compresi tra 1 e -1. 
 # per valori maggiori di 0.3 si assegna il pixel ad uno specchio d'acqua. Viene utilizzato proprio per quantificare l'innalzamento del livello d'acqua.
 
-dif1 = lbdo[[2]]-lbdo[[4]] # differenza tra GREEN e NIR
-sum1 = lbdo[[2]]+lbdo[[4]] # somma tra GREEN e NIR
-nwat1= dif1/sum1 
-plot(nwat1, col=viridis (1000))
+dif1 = l17[[2]] - l17[[4]] # differenza tra GREEN e NIR
+sum1 = l17[[2]] + l17[[4]] # somma tra GREEN e NIR
+ndwi1= dif1 / sum1 
+plot(ndwi1, col=viridis (1000))
 
-dif2 = dbdo[[2]]-dbdo[[4]]
-sum2 = dbdo[[2]]+dbdo[[4]]
-nwat2= dif2/sum2
-plot(nwat2, col=viridis (1000))
+dif2 = d17[[2]] - d17[[4]]
+sum2 = d17[[2]] + d17[[4]]
+ndwi2= dif2 / sum2
+plot(ndwi2, col=viridis (1000))
 
 # sfrutto la classificazione per poter quantificare l'incremento della superficie idrica, paragonando le percentuali ottenute e assegnate all'acqua.
-nwat1c <- im.classify(nwat1, num_clusters=4)
+l17c <- im.classify(ndwi1, num_clusters=4)
 
 # classe 1: Altro                9.04   %
 # classe 2: Suolo                21.12  %
 # classe 3: Vege.                69.62  %
 # classe 4: Acqua                0.22   %
 
-nwat2c <- im.classify(nwat2, num_clusters=4)
+d17c <- im.classify(ndwi2, num_clusters=4)
 
 # classe 1: Altro                15.13  %
 # classe 2: Acqua                0.89   %
 # classe 3: Suolo                61.30  %
 # classe 4: Vege.                22.67  %
 
-tot1 <- ncell(nwat1c)
-f1 <- freq(nwat1c)
+tot1 <- ncell(l17c)
+f1 <- freq(l17c)
 prop1 = f1 / tot1
 perc1 = prop1 * 100 # trasformo i dati in percentuali
 
-tot2 <- ncell(nwat2c)
-f2 <- freq(nwat2c)
+tot2 <- ncell(d17c)
+f2 <- freq(d17c)
 prop2 = f2 / tot2
 perc2 = prop2 * 100
 
 # creo una tabella in cui verranno inseriti i dati relativi ad ogni classe
 
-class <- c("Altro", "Suolo", "Vege.", "Acqua")
-pwat1 <- c(9.04, 21.12, 69.62, 0.22)
-pwat2 <- c(15.13, 61.30, 22.67, 0.89)
+class <- c("water", "soil", "Fol. Cov.", "Grass")
+pl17 <- c(2.13, 11.70, 31.67, 54.50)
+pd17 <- c(7.22, 19.18, 9.80, 63.79)
 
-tabout <- data.frame(class, pwat1, pwat2)
-tabout
+tab <- data.frame(class, pl17, pd17)
+tab
 
-p1 <- ggplot(tabout, aes(x=class, y=pwat1, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100)) # imposto il grafico per mostrare i risultati
-p2 <- ggplot(tabout, aes(x=class, y=pwat2, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
+p1 <- ggplot(tab, aes(x=class, y=pl17, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100)) # imposto il grafico per mostrare i risultati
+p2 <- ggplot(tab, aes(x=class, y=pd17, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p1 + p2
 
